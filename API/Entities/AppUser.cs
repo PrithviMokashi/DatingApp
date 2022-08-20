de3082b25ffec1393caa64e760d9fd0f119b8e3e
PrithviMokashi <mokashiprithviraj@gmail.com>
2022-08-20 22:08:21 +0530

    Extending API

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extentions;
 
 namespace API.Entities
 {
@@ -10,9 +11,24 @@ namespace API.Entities
         //Adds the data to sqlite database using Entity Framework
         public int Id { get; set; }
         public string UserName { get; set; }
-        
         public byte[] PasswardHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        //One to many relationship 1 user many Photos
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,26 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
-        public RequestDelegate Next { get; }
-        private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, 
             IHostEnvironment env)
         {
             _env = env;
-            _next = next;
             _logger = logger;
+            _next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
